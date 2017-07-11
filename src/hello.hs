40308e945dd25fce7d538d34c0c2a2f0353333c0
Bijan <bijanbwb@gmail.com>
2017-07-11 17:33:20 -0400

    Hello world example

---
+++ src/hello.hs
@@ -0,0 +1,10 @@
+-- hello.hs
+--
+-- Run from the command line with the following:
+--   $ ghci hello.hs
+--   > greeting
+--   "Hello World"
+hello = "Hello"
+world = "World"
+
+greeting = hello ++ " " ++ world
