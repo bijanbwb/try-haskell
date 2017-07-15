e6d134e9b78756ccfe9159f707eb4b002f3d7344
Bijan <bijanbwb@gmail.com>
2017-07-15 11:23:25 -0400

    Add comments to test example

--- src/test.hs
+++ src/test.hs
@@ -1,5 +1,11 @@
 -- sayHello.hs
+--
 -- First example from the Haskell Programming book.
+-- Run from the command line with the following:
+--   $ stack ghci
+--   > :load test.hs
+--   > sayHello "Haskell"
+--   Hello, Haskell!
 
 sayHello :: String -> IO ()
 sayHello x = putStrLn ("Hello, " ++ x ++ "!")
