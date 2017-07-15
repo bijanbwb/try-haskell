40e32a0e72f19d034854460dc0ee5d9b4995bbfa
Bijan <bijanbwb@gmail.com>
2017-07-15 12:25:53 -0400

    Update squarepiify function to use base pi

--- src/squarepiify.hs
+++ src/squarepiify.hs
@@ -7,8 +7,8 @@
 --   $ stack ghci
 --   > :load squarepiify.hs
 --   > squarepiify 5
---   78.5
+--   78.53981633974483
 
 -- squarepiify: square a number and then multiply by pi.
-squarepiify x = 3.14 * (x * x)
+squarepiify x = pi * (x * x)
 
