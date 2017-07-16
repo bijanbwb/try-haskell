89590feef0cf9f5f1473929390b4da0fbd28dbb3
Bijan <bijanbwb@gmail.com>
2017-07-16 00:35:50 -0400

    Add precedence example

---
+++ src/learn.hs
@@ -0,0 +1,20 @@
+-- learn.hs
+--
+-- Run from the command line with the following:
+--   $ stack ghci
+--   > :load learn.hs
+--   > x
+--   60
+--   > y
+--   10
+--   > myResult
+--   300
+
+module Learn where
+
+x = 10 * 5 + y
+
+myResult = x * 5
+
+y = 10
+
