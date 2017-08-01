7176f43124f75f521d7868fbbb9136d18c3889b4
Bijan <bijanbwb@gmail.com>
2017-08-01 16:42:09 -0400

    Refactor with streams

--- projects/baseball/src/Main.hs
+++ projects/baseball/src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
-import Data.Csv
+import qualified Data.Foldable as F
+import Data.Csv.Streaming
 
 -- TYPES
 
@@ -13,7 +13,7 @@ type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 fourth :: (a, b, c, d) -> d
 fourth (_, _, _, d) = d
 
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats :: BL.ByteString -> Records BaseballStats
 baseballStats = decode NoHeader
 
 -- MAIN
@@ -21,7 +21,7 @@ baseballStats = decode NoHeader
 main :: IO ()
 main = do
   csvData <- BL.readFile "batting.csv"
-  let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+  let summed = F.foldr summer 0 (baseballStats csvData)
   putStrLn $ "Total at bats was: " ++ (show summed)
   where summer = (+) . fourth
 
