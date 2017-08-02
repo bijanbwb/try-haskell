02a33cc0ca1de587bd14d03d43b14957e863ce74
Bijan <bijanbwb@gmail.com>
2017-08-02 09:08:17 -0400

    Adding location for H-99 problems

---
+++ exercises/h-99/01.hs
@@ -0,0 +1,15 @@
+{-
+
+H-99 Problem 1: Find the last element of a list.
+
+Examples:
+> myLast [1,2,3,4]
+4
+> myLast ['x','y','z']
+'z'
+
+-}
+
+main :: IO ()
+main = do
+    putStrLn "hello world"
