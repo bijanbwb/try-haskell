e14bc2e6800eeac2db90d25e9368ea607b5e8879
Bijan <bijanbwb@gmail.com>
2017-08-02 09:30:40 -0400

    Initial solution for first H-99 problem

--- exercises/h-99/01.hs
+++ exercises/h-99/01.hs
@@ -3,13 +3,34 @@
 H-99 Problem 1: Find the last element of a list.
 
 Examples:
-> myLast [1,2,3,4]
+> myLast [1, 2, 3, 4]
 4
-> myLast ['x','y','z']
+> myLast ['x', 'y', 'z']
 'z'
 
+Usage:
+$ runhaskell 01.hs
+✅ myLast [1, 2, 3, 4] == 4 
 -}
+
+-- myLast
+
+myLast :: [a] -> a
+myLast xs = head $ reverse xs
+
+-- HELPERS
+
+displayResult :: Bool -> String
+displayResult bool =
+    case bool of
+        True ->
+            "✅"
+        False ->
+            "❌"
+
+-- MAIN
 
 main :: IO ()
 main = do
-    putStrLn "hello world"
+    let result = myLast [1, 2, 3, 4] == 4 in
+        putStrLn $ displayResult result ++ "  myLast [1, 2, 3, 4] == 4"
