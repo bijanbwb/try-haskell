f99a4f58c6203b44dbc446b21ef071a9dfd3cf6c
Bijan <bijanbwb@gmail.com>
2017-08-18 17:59:48 -0400

    Creating a scratch file for Haskell testing

--- src/scratch.hs
+++ src/scratch.hs
@@ -1,7 +1,21 @@
+{-
+    Scratch module for testing out various code snippets.
+    Some Haskell features can't be learned in the REPL.
+-}
 module Scratch where
+
+-- DECLARATIONS
 
 hello :: String
 hello = "hello"
+
+x :: Int
+x = 3
+
+y :: Int
+y = y + 1
+
+-- MAIN
 
 main :: IO ()
 main = do
