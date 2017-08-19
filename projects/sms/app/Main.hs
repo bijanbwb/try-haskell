d08d32cb834d71ce3cad9bd4562d9227449a2fd7
Bijan <bijanbwb@gmail.com>
2017-08-19 09:27:16 -0400

    Add initial Twilio code

--- projects/sms/app/Main.hs
+++ projects/sms/app/Main.hs
@@ -1,6 +1,14 @@
+{- #LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Control.Monad.IO.Class(liftIO)
+import System.Environment (getEnv)
+import Twilio
+import Twilio.Messages
 
-main :: IO ()
-main = someFunc
+main:: IO()
+main = runTwilio' (getEnv "TWILIO_ACCOUNT_SID")
+                  (getEnv "TWILIO_AUTH_TOKEN") $ do
+  let body = PostMessage "3059154770" "3212048454" "Hello, World!"
+  message <- post body
+  liftIO $ print message
