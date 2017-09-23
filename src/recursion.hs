4cecd21dfdf3fdaf087e53d5bbac97a12b617206
Bijan <bijanbwb@gmail.com>
2017-09-23 15:53:05 -0400

    Add recursive examples

--- src/recursion.hs
+++ src/recursion.hs
@@ -30,3 +30,20 @@ double nums =
   if null nums
   then []
   else (2 * (head nums)) : (double (tail nums))
+
+-- Recursively calculate the length of a list.
+lengthy :: [a] -> Int
+lengthy [] = 0
+lengthy (x:xs) = 1 + length xs
+
+-- Recursively filter through a list.
+filtery :: (a -> Bool) -> [a] -> [a]
+filtery pred [] = []
+filtery pred (x:xs)
+    | pred x = x : filtery pred xs
+    | otherwise = filtery pred xs
+
+-- Recursively map through a list.
+mappy :: (a -> b) -> [a] -> [b]
+mappy _ [] = []
+mappy f (x:xs) = f x : mappy f xs
